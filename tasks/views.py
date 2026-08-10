b9861092e059e9f11a3787463ae3c7964cfcac94
lordcroakster <alexwinter2004xx@gmail.com>
2026-08-10 17:02:43 +0100

    added CRUD API for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,116 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .models import Task
+from projects.models import Project
+from django.http import JsonResponse
+import json
+
 
 # Create your views here.
+@csrf_exempt
+def task_list_create(request, project_id):
+    if request.method != "GET" and request.method != "POST":
+        return JsonResponse({
+                    "error": "Method not allowed"
+                }, status=405)
+
+    try:
+        project=Project.objects.get(id=project_id)
+    except Project.DoesNotExist:
+        return JsonResponse({
+            "error": "Project does not exist."
+        }, status=404)
+
+    if request.method == "GET":
+        tasks=Task.objects.filter(project_id=project_id)
+        
+        data=[]
+        for task in tasks:
+            data.append({
+                    "task_id": task.id,
+                    "name": task.name
+                })
+
+        return JsonResponse(data, safe=False)
+
+    if request.method == "POST":
+        body=json.loads(request.body)
+        new_task=Task.objects.create(
+            name=body["name"],
+            description=body["description"],
+            completed=False,
+            project=project
+        )
+
+        return JsonResponse({
+            "message": "Task created",
+            "id": new_task.id,
+            "name": new_task.name
+        }, status=201)
+
+@csrf_exempt
+def task_detail(request, project_id, task_id):
+    if request.method != "GET" and request.method != "DELETE" and request.method != "PATCH":
+        return JsonResponse({
+            "error": "Method not allowed"
+            }, status=405)
+
+    try:
+        task=Task.objects.get(id=task_id, project_id=project_id)
+    except Task.DoesNotExist:
+        return JsonResponse({
+            "error": "Task does not belong to this project/project doesn't exist"
+        }, status=404)
+
+
+    if request.method == "GET":
+        return JsonResponse({
+            "id": task.id,
+            "name": task.name,
+            "description": task.description,
+            "completed": task.completed,
+            "created_at": task.created_at,
+        })
+
+    if request.method == "DELETE":
+        task.delete()
+
+        return JsonResponse({
+            "message": "Task deleted"
+        }, status=200)
+
+    if request.method == "PATCH":
+        try:
+            body=json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({
+                "error": "Invalid JSon"
+            }, status=400)
+
+        if "name" in body:
+            if body["name"] == "":
+                return JsonResponse({
+                    "error": "Task must have a name"
+                }, status=400)
+
+            task.name=body["name"]
+
+        if "description" in body:
+            task.description=body["description"]
+
+        if "completed" in body:
+            if not isinstance(body["completed"], bool):
+                return JsonResponse({
+                    "error": "Completed field must contain a boolean"
+                }, status=400)
+            task.completed=body["completed"]
+
+        task.save()
+        return JsonResponse({
+            "message": "Updated successfully",
+            "task_id": task.id,
+            "name": task.name,
+            "description": task.description,
+            "completed": task.completed
+        }, status=200)
+
